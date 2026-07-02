66f6fea107357db58d2e5b29653ef56625d48b42
Your Name <you@example.com>
2026-07-02 21:30:38 +0900

    フィードバックのメッセージを1秒毎に表示する調整をしました。

--- app/javascript/controllers/chat_feedback_controller.js
+++ app/javascript/controllers/chat_feedback_controller.js
@@ -4,6 +4,12 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "message" ]
 
+  connect() {
+    setTimeout(() => {
+      this.removeHiddenOneByOne()
+        }, 2000)
+  }
+
   removeFirst() {
     // 1番上のメッセージ要素を取得 (配列の0番目)
     const firstMessage = this.messageTargets[0]
@@ -13,4 +19,29 @@ export default class extends Controller {
       firstMessage.remove()
     }
   }
+
+  removeHiddenOneByOne() {
+    const lastIndex = this.messageTargets.length - 1;
+
+    this.messageTargets.forEach((message, index) => {
+      setTimeout(() => {
+        message.classList.remove("hidden");
+
+        // もし現在の要素が「最後の要素」だったら処理を実行
+        if (index === lastIndex) {
+          this.afterAllMessagesShown(message);
+        }
+
+      }, index * 1000);
+    });
+  }
+
+  // すべて表示された後に実行したい処理
+  afterAllMessagesShown(lastMessage) {
+    console.log("すべてのメッセージが表示されました！");
+    // 例：最新のメッセージまで自動スクロールする
+    lastMessage.scrollIntoView({ behavior: "smooth" });
+    lastMessage.classList.add("hidden");
+  }
+  
 }
