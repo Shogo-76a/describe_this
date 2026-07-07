d6cb883399f31ada692d0e4c1f66b6bb275cc053
Your Name <you@example.com>
2026-07-07 21:00:30 +0900

    allow_browser versions: :modernのコメントアウトを消して、有効にしました。本番環境でのみ有効にしています。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,8 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  #---------本番環境ではコメントアウト消して、有効にしておくこと----------#
-  #  allow_browser versions: :modern
-  #---------本番環境ではコメントアウト消して、有効にしておくこと----------#
-
+  if Rails.env.production?
+    allow_browser versions: :modern
+  end
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
