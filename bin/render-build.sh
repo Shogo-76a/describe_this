3bc2c15764d17421456fe183c469c821d1750a1f
Your Name <you@example.com>
2026-07-10 20:24:08 +0900

    bin/render-build.shのコマンド記述を修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -9,10 +9,10 @@ bundle exec rails assets:precompile
 bundle exec rails assets:clean
 
 # 1. メイン（Neon）のマイグレーションを実行
-bin/rails exec rails db:migrate
+bundle exec rails db:migrate
 
 # 2. Render内部DB（cache）にテーブルを一括作成
-bin/rails exec rails db:schema:load:cache SCHEMA=db/cache_schema.rb
+bundle exec rails db:schema:load:cache SCHEMA=db/cache_schema.rb
 
 # 3. Render内部DB（queue）にテーブルを一括作成
-bin/rails exec rails db:schema:load:queue SCHEMA=db/queue_schema.rb
+bundle exec rails db:schema:load:queue SCHEMA=db/queue_schema.rb
