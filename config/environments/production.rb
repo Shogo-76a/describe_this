78be1e6fd0a0b51515dab2811f03f76dfa344a7a
Your Name <you@example.com>
2026-07-10 20:17:07 +0900

    config/cache.ymlのproduction設定のdatabase: cacheを削除

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,8 +54,9 @@ Rails.application.configure do
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
   config.active_job.queue_adapter = :solid_queue
-  config.solid_queue.connects_to = { database: { writing: :queue } }
   # Solid Queueの設定
+  config.solid_queue.connects_to = { database: { writing: :queue, reading: :queue } }
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
