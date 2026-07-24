cf3f85bd21eda57f7bd0670d68cdb2c4f7742fcf
Your Name <you@example.com>
2026-07-24 19:12:26 +0900

    controllerとserviceのRspecを追加しました。

--- app/services/cloudinary_folder_service.rb
+++ app/services/cloudinary_folder_service.rb
@@ -3,9 +3,9 @@ class CloudinaryFolderService
     # 頻繁なAPIリクエストを防ぐため、1時間キャッシュする
     Rails.cache.fetch("cloudinary_folder_#{folder_name}", expires_in: 1.hour) do
       # CloudinaryのSearch APIを呼び出す
-      result = Cloudinary::Search
+      result = Cloudinary::Search.new
                 .expression("folder:#{folder_name} AND resource_type:image")
-                .max_results(50) # 必要に応じて調整
+                .max_results(25) # 必要に応じて調整
                 .execute
 
       # 画像の public_id（または外部URL）の配列を返す
