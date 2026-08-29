91ad8ca1c4887fc21703def457535c9c0b0beba6
Your Name <you@example.com>
2026-08-29 15:22:24 +0900

    Rspecの不具合修正

--- spec/system/game_cycle_spec.rb
+++ spec/system/game_cycle_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe 'ログイン後 ゲームメインサイクル', type: :system d
       expect(page).to have_css('img')
 
       # --- 画像生成ページで説明文を入力して送信（update に相当） ---
-      expect(page).to have_content("お題を 英語で 説明してください")
+      expect(page).to have_content("お題を 英語で 説明してください", wait: 10)
       fill_in 'game_description', with: 'a coffe cup on a tablu.' # スペルミスを意図的に含む coffe:coffee, tablu:table
       find('button.btn-primary.d-inline-flex').click # 送信ボタン
 
