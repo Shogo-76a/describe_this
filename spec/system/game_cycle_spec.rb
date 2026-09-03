eac9667d8b49abcff7dadd90050d6b1ddd002f71
Your Name <you@example.com>
2026-09-03 14:55:27 +0900

    Rspecを調整しました。ゲームサイクルのRspecではゲーム内言語のcookieを作成してテスト。

--- spec/system/game_cycle_spec.rb
+++ spec/system/game_cycle_spec.rb
@@ -12,6 +12,10 @@ RSpec.describe 'ログイン後 ゲームメインサイクル', type: :system d
   context '生成画像がある場合' do
     it '開始画面から画面3のAPIデータ変換・更新まで一連の流れが正しく機能すること', vcr: { cassette_name: 'game_cycle_flow' }, js: true do
       # --- ゲーム開始画面 ---
+      find('.dropdown.w-30').click
+      find('.dropdown-content.menu a', text: '英語').click
+      expect(page).to have_css('.btn-sm.select', text: '英語')
+      
       click_button 'はじめる' # root -> new をトリガ
       expect(page).to have_content("お題", wait: 5)
       expect(page).to have_button("つぎへ")
--- spec/system/guest/game_cycle_spec.rb
+++ spec/system/guest/game_cycle_spec.rb
@@ -4,6 +4,14 @@ RSpec.describe 'ゲスト ゲームメインサイクル', type: :system do
   # ゲーム開始から画像生成・更新までの一連を1カセットにまとめる想定
   context '生成画像がある場合' do
     it '開始画面から画面3のAPIデータ変換・更新まで一連の流れが正しく機能すること', vcr: { cassette_name: 'guest_game_cycle_flow' }, js: true do
+      visit root_path
+      page.refresh
+      find('.modal').send_keys(:escape) # ポップアップを消す
+      
+      find('.dropdown.w-30').click
+      find('.dropdown-content.menu a', text: '英語').click
+      expect(page).to have_css('.btn-sm.select', text: '英語')
+      
       # --- ゲーム開始画面 ---
       visit new_guest_game_path
       page.refresh # 導入画面をスキップ
