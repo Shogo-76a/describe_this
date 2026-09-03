32b460af0800fccdae32bc0075a8a10ab68c50eb
Your Name <you@example.com>
2026-09-03 10:05:41 +0900

    トップページのヘルパーメソッドをrender_locale_nameをapplication_helperに移動して他ページでも呼び出せるようにしました。

--- app/helpers/top_helper.rb
+++
@@ -1,8 +0,0 @@
-# トップページ用のヘルパーメソッド置き場
-module TopHelper
-  # スコアに応じて テキストとHTMLの見た目を返すHTMLを作成する。
-  def render_locale_name(target_locale)
-    TextSelectorOnLocale.call(target_locale)
-    # 文字列が返る
-  end
-end
